63b7a535664499b843b7c0310d9174d0f5068e6f
Michael Landreh <michael.landreh@gmail.com>
2017-05-16 20:14:10 +0200

    Updated Concerts model

--- app/controllers/concerts_controller.rb
+++ app/controllers/concerts_controller.rb
@@ -1,5 +1,25 @@
 class ConcertsController < ApplicationController
   def index 
+    @concerts = Concert.all
+  end
+  
+  def new
     
   end
+  
+  def show
+    @concert = Concert.find(params[:id])
+  end
+  
+  def create
+    @concert= Concert.new(concert_params)
+    
+    @concert.save
+    redirect_to @concert
+  end
+  
+  private
+    def concert_params
+      params.require(:concert).permit(:title,:text, :subTitle, :location, :eventDate, :category, :eventTime, :eventDateTitle, :published)
+    end
 end
