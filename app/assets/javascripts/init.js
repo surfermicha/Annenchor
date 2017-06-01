966d0232c80ea4e563defae95ce27ad5a6a611b7
Michael Landreh <michael.landreh@gmail.com>
2017-06-01 17:37:21 +0200

    Added Masterslider and Fancybox plugin

--- app/assets/javascripts/init.js
+++ app/assets/javascripts/init.js
@@ -1,4 +1,6 @@
 //This file contains all initialization expressions for Javascript
 jQuery(document).ready(function() {
     App.init();
+    FancyBox.initFancybox();
+    MSfullWidth.initMSfullWidth();
 });
